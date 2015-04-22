a514540ecf552b0ae3fb968d733d3a9b6b014e8e
Mariusz Wojcik <mariusz.wojcik@hotmail.co.uk>
2015-04-22 22:00:59 +0100

    Login user

---
+++ OwinRefreshTokenExample/WebApi.IntegrationTests/AuthorizingUserSpecs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using NUnit.Framework;
+using Should;
+using SpecsFor;
+
+namespace WebApi.IntegrationTests
+{
+    [Explicit("Requires WebApi project to run in the background.")]
+
+    public class AuthorizingUserSpecs : SpecsFor<TestWebClient>
+    {
+        private string _result;
+
+        protected override void When()
+        {
+            _result = SUT.Login("a1@b.com", "Password1!");
+        }
+
+        [Test]
+        public void then_should_load_data_successfully()
+        {
+            SUT.LastOperationHttpStatusCode.ShouldEqual(HttpStatusCode.OK);
+            Console.WriteLine(SUT.LastOperationResponse);
+        }
+
+    }
+}
--- OwinRefreshTokenExample/WebApi.IntegrationTests/TestWebClient.cs
+++ OwinRefreshTokenExample/WebApi.IntegrationTests/TestWebClient.cs
@@ -1,25 +1,32 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
 
 namespace WebApi.IntegrationTests
 {
     public class TestWebClient : WebClient
     {
         private const string WebApiUrl = @"http://localhost:6098/";
+        private const string TokenEndpointUrl = @"https://localhost:44301/token";
 
         public HttpStatusCode LastOperationHttpStatusCode { get; private set; }
+
+        public string LastOperationResponse { get; private set; }
 
         private string LoadData(string path)
         {
             try
             {
-                var result = DownloadString(WebApiUrl + path);
+                LastOperationResponse = DownloadString(WebApiUrl + path);
                 LastOperationHttpStatusCode = HttpStatusCode.OK;
-                return result;
+                return LastOperationResponse;
             }
             catch (WebException e)
             {
                 var response = (HttpWebResponse) e.Response;
                 LastOperationHttpStatusCode = response.StatusCode;
+                LastOperationResponse = string.Empty;
                 return string.Empty;
             }
         }
@@ -32,6 +39,38 @@ namespace WebApi.IntegrationTests
         public string LoadProtectedData()
         {
             return LoadData("SecurityTest/protected");
+        }
+
+        public string Login(string username, string password)
+        {
+            try
+            {
+                var data = new NameValueCollection
+                {
+                    {"grant_type", "password"},
+                    {"username", username},
+                    {"password", password}
+                };
+                Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                var result = UploadValues(TokenEndpointUrl, data);
+                LastOperationHttpStatusCode = HttpStatusCode.OK;
+                LastOperationResponse = System.Text.Encoding.Default.GetString(result);
+                return LastOperationResponse;
+            }
+            catch (WebException e)
+            {
+                var response = (HttpWebResponse)e.Response;
+                LastOperationHttpStatusCode = response.StatusCode;
+                using (var s = response.GetResponseStream())
+                using (var sr = new StreamReader(s))
+                {
+                    LastOperationResponse = sr.ReadToEnd();
+                    Console.WriteLine(LastOperationResponse);
+                }
+                return string.Empty;
+            }
+
+            
         }
     }
 }
